7d15075f844645d4d7b3161cb1f29f344a6ce81f
Changhua <lichanghua0821@gmail.com>
2023-01-02 23:26:57 +0800

    Add otto dataset

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -4,12 +4,14 @@ from .ml_10m import ML10MDataset
 from .yelp import YelpDataset
 from .retail import RetailDataset
 from .ijcai import IjcaiDataset
+from .otto import OttoDataset
 
 DATASETS = {
     ML10MDataset.code(): ML10MDataset,
     YelpDataset.code(): YelpDataset,
     RetailDataset.code(): RetailDataset,
-    IjcaiDataset.code(): IjcaiDataset
+    IjcaiDataset.code(): IjcaiDataset,
+    OttoDataset.code(): OttoDataset
 }
 
 
---
+++ src/datasets/otto.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+from .base import AbstractDataset
+
+import pandas as pd
+
+
+class OttoDataset(AbstractDataset):
+    @classmethod
+    def code(cls):
+        return "otto"
+
+    def load_df(self):
+        folder_path = self._get_rawdata_root_path()
+        file_path = folder_path.joinpath("otto.txt")
+        df = pd.read_csv(file_path, sep="\t", header=None)
+        df.columns = ["uid", "sid", "behavior", "timestamp"]
+        return df
